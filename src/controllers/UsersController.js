ce2ddb7a1768da1af24a691723d918220dcd5ac4
Nishiyama13 <nishiyamaeel@gmail.com>
2023-03-06 09:55:53 -0300

    signUp

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,6 +6,24 @@ export async function signUp(req, res) {
   const { name, email, password } = req.body;
 
   try {
+    const checkExistingUser = await db.query(
+      `SELECT * FROM users WHERE email = $1`,
+      [email]
+    );
+
+    if (checkExistingUser.rowCount > 0) return res.sendStatus(409);
+
+    const passwordHash = bcrypt.hashSync(password, 10);
+
+    await db.query(
+      `
+    INSERT INTO users (name, email, password) 
+    VALUES ($1, $2, $3)
+    `,
+      [name, email, passwordHash]
+    );
+
+    res.sendStatus(201);
   } catch (error) {
     return res.status(500).send(error.message);
   }
--- src/routers/index.routers.js
+++ src/routers/index.routers.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import userRouter from "./user.routers.js";
+import usersRouter from "./users.routers.js";
 //import urlsRouter from "./urls.routers.js";
 //import  from "./.routers.js"
 
 const router = Router();
 
-router.use(userRouter);
+router.use(usersRouter);
 //router.use(urlsRouter);
 //router.use();
 
